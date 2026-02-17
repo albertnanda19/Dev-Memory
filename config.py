baeea95b2ed9dd44bfbc6a8de2ef06ab67959747
albert19_ <albertnanda1902@gmail.com>
2026-02-18 03:45:22 +0700

    feat: add another repository

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ def get_repo_paths() -> list[str]:
     return [
         "/Users/albert-dibimbing/Documents/coding-repository/edlink-cakrawala",
         "/Users/albert-dibimbing/Documents/coding-repository/spmb-cakrawala-be",
+        "/Users/albert-dibimbing/Documents/coding-repository/spmb-cakrawala",
     ]
 
